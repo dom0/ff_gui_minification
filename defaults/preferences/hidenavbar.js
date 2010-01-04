0b0f8ef2f50e53a41d50fc59dcd60301bfe4e2f7
Domenico Martella <domenico.martella@alcacoop.it>
2010-01-04 12:23:10 +0100

    Initial import

---
+++ defaults/preferences/hidenavbar.js
@@ -0,0 +1,8 @@
+pref("extensions.hidenavbar@jaybaldwin.description", "chrome://hidenavbar/locale/hidenavbar.properties");
+pref("hidenavbar.autohide", false);
+pref("hidenavbar.autohidedelay", 1000);
+pref("hidenavbar.autohideshowdelay", 1000);
+pref("hidenavbar.enabled", true);
+pref("hidenavbar.hideonstart", 2); // 0 = not hidden, 1 = hidden, 2 = remember last
+pref("hidenavbar.hidden", false);
+pref("hidenavbar.keycode", 113);
